e3150674365ec93586048e0e7fc151abbab11eab
アニップ <Hanivan@users.noreply.github.com>
2023-02-01 17:53:20 +0700

    fix: instagram response model

--- apps/downloader/src/dtos/payloads/instagram.payload.ts
+++ apps/downloader/src/dtos/payloads/instagram.payload.ts
@@ -1,10 +1,14 @@
 import { BaseResponse } from './base.payload';
 
+export interface LinkInstagramResponse {
+  url: string;
+}
+
 export interface InstagramResponse extends BaseResponse {
   data: {
     id: string;
-    link_downloads: string[];
-    link_previews: string[];
+    link_downloads: LinkInstagramResponse[];
+    link_previews: LinkInstagramResponse[];
     format: string;
   };
 }
